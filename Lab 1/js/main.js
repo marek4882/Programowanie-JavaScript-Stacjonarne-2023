76fa6adc54a01ebc9f56c05987c34ed25005807b
cwior <cwioro200@gmail.com>
2023-10-12 23:02:40 +0200

    Wersja zieew 1.0

--- Lab 1/js/main.js
+++ Lab 1/js/main.js
@@ -1,19 +1,40 @@
-document.getElementById("calculate-button").addEventListener("click", function() {
-    let value1 = parseFloat(document.getElementById("value1").value) || 0;
-    let value2 = parseFloat(document.getElementById("value2").value) || 0;
-    let value3 = parseFloat(document.getElementById("value3").value) || 0;
-    let value4 = parseFloat(document.getElementById("value4").value) || 0;
+let livechange = false; 
+function oblicz() {
+  let value1 = parseFloat(document.getElementById("value_1").value) || 0;
+  let value2 = parseFloat(document.getElementById("value_2").value) || 0;
+  let value3 = parseFloat(document.getElementById("value_3").value) || 0;
+  let value4 = parseFloat(document.getElementById("value_4").value) || 0;
 
-    let sum = value1 + value2 + value3 + value4;
-    let average = sum / 4;
-    let min = Math.min(value1, value2, value3, value4);
-    let max = Math.max(value1, value2, value3, value4);
+  let sum = value1 + value2 + value3 + value4;
+  let average = sum / 4;
+  let min = Math.min(value1, value2, value3, value4);
+  let max = Math.max(value1, value2, value3, value4);
 
-    document.getElementById("sum-result").textContent = sum;
-    document.getElementById("average-result").textContent = average;
-    document.getElementById("min-result").textContent = min;
-    document.getElementById("max-result").textContent = max;
+  document.getElementById("sum_result").textContent = sum;
+  document.getElementById("average_result").textContent = average;
+  document.getElementById("min_result").textContent = min;
+  document.getElementById("max_result").textContent = max;
 
-    let boxResult = document.getElementById("results");
-    boxResult.style.display="block"
+  let boxResult = document.getElementById("results");
+  boxResult.style.display = "block";
+}
+
+document.getElementById("calculate_button").addEventListener("click", function(){
+    livechange = true;
+    oblicz();
+});
+
+const inputFields = [
+  document.getElementById("value_1"),
+  document.getElementById("value_2"),
+  document.getElementById("value_3"),
+  document.getElementById("value_4"),
+];
+
+inputFields.forEach(function (inputField) {
+  inputField.addEventListener("input", function(){
+    if(livechange == true){
+        oblicz();
+    }
+  });
 });
