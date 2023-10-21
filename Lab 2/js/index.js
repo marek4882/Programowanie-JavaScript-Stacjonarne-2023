bb00f837abcc347aa928c34d94bbbaa6ffbd3dab
marek <cwioro200@gmail.com>
2023-10-21 15:38:32 +0200

    Wersja anacotokomu 2.0

---
+++ Lab 2/js/index.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll('.slide');
+  let dots = document.querySelectorAll('.dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'flex';
+  dots[slideIndex - 1].className += ' active';
+}
+
+let slideInterval = setInterval(function() {
+  plusSlides(1);
+}, 3000); 
+
+
+
+
