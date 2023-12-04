45709ce88a25fc3caffc649e2f829cf7a8d53338
marek4882 <cwioro200@gmail.com>
2023-12-04 20:29:29 +0100

    Lab 2 Wersja da sie ludziom pokazać

--- Lab 2/js/index.js
+++ Lab 2/js/index.js
@@ -1,4 +1,6 @@
 let slideIndex = 1;
+let slideInterval;
+
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -11,8 +13,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.querySelectorAll('.slide');
-  let dots = document.querySelectorAll('.dot');
+  let slides = document.querySelectorAll(".slide");
+  let dots = document.querySelectorAll(".dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -20,19 +22,34 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = 'flex';
-  dots[slideIndex - 1].className += ' active';
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
 }
 
-let slideInterval = setInterval(function() {
-  plusSlides(1);
-}, 3000); 
+function startSlider() {
+  slideInterval = setInterval(function () {
+    plusSlides(1);
+  }, 3000);
+}
 
+function pauseSlider() {
+  clearInterval(slideInterval);
+}
 
+startSlider();
 
+let pauseButton = document.getElementById("pauseButton");
+let resumeButton = document.getElementById("resumeButton");
 
+pauseButton.addEventListener("click", function () {
+  pauseSlider();
+});
+
+resumeButton.addEventListener("click", function () {
+  startSlider();
+});
